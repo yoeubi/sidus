3bd2d24a522b2490d8a53586c074a454fa9975fc
JungwonSeong <yoeubi28@gmail.com>
2019-05-01 18:25:46 +0900

    fix: return 추가

--- routes/join.js
+++ routes/join.js
@@ -7,7 +7,7 @@ router.post("/", async (req, res, next) => {
     try {
         const exUser = await User.findOne({ where: { email } });
         if (exUser) {
-            res.status(408).json("이미 가입된 이메일입니다.");
+            return res.status(408).json("이미 가입된 이메일입니다.");
         }
         await User.create({
             email
